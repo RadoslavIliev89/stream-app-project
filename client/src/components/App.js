a256ec78dfb2ae35998f5df6c807f2ec4f936d97
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-06-17 16:06:20 +0300

    Added router and semantic

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import StreamCreate from './Streams/StreamCreate';
+import StreamDelete from './Streams/StreamDelete';
+import StreamEdit from './Streams/StreamEdit';
+import StreamList from './Streams/StreamList';
+import StreamShow from './Streams/StreamShow';
+
+
+const App =()=>{
+return(
+    <div>
+        <BrowserRouter>
+        <div>
+            <Route path="/" exact component={StreamList}/>
+            <Route path="/streams/new"  component={StreamCreate}/>
+            <Route path="/streams/edit"  component={StreamEdit}/>
+            <Route path="/streams/delete"  component={StreamDelete}/>
+            <Route path="/streams/shows"  component={StreamShow}/>
+        </div>
+        </BrowserRouter>
+    </div>
+)
+}
+export default App;
