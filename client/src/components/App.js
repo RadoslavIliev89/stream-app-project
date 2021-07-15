f1004eed4405e979b64a70a623b954fc103c80f0
RadoslavIliev89 <radoslaviliev63@gmail.com>
2021-07-15 15:05:53 +0300

    added rtmp server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import {Router, Route} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
@@ -9,20 +9,22 @@ import Header from './Header';
 import history from '../history'
 
 
-const App =()=>{
-return(
-    <div className="ui container">
-        <Router history={history}>
-        <div>
-        <Header/>
-            <Route path="/" exact component={StreamList}/>
-            <Route path="/streams/new"  component={StreamCreate}/>
-            <Route path="/streams/edit/:id"  component={StreamEdit}/>
-            <Route path="/streams/delete/:id"  component={StreamDelete}/>
-            <Route path="/streams/:id"  component={StreamShow}/>
+const App = () => {
+    return (
+        <div className="ui container">
+            <Router history={history}>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>
+                </div>
+            </Router>
         </div>
-        </Router>
-    </div>
-)
+    )
 }
 export default App;
